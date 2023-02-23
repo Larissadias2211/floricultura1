912365fa394181c51c1b00037c69d6382c5e05fb
larissa dias <110576062+Larissadias2211@users.noreply.github.com>
2023-02-22 22:19:16 -0300

    flores

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Floricultura</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Floricultura</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active"  href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contatos</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/flower.jpg" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="img/flower2.jpg" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="img/flower3.jpg" class="d-block w-100">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+
+          coluna01
+
+        <div class="card">
+  <img src="img/flower.jpg" class="card-img-top" alt="Foto de imagem">
+  <div class="card-body">
+    <h5 class="card-title">Arranjo de flor</h5>
+    <p class="card-text">No nosso site voce encontra os melhores arranjo de flores</p>
+    <a href="#" class="btn btn-Danger btn-lg">Compre já</a>
+  </div>
+</div>
+
+</div>
+<div class="col">Card02
+<div class="card">
+  <img src="img/flower2.jpg" class="card-img-top" alt="Foto de uma imagem">
+  <div class="card-body">
+    <h5 class="card-title text-success">Arranjo de flor</h5>
+    <p class="card-text">No nosso site voce encontra os melhores arranjo de flores</p>
+    <a href="#" class="btn btn-outline-Danger btn-lg">Compre já</a>
+  </div>
+</div>
+
+</div>
+<div class="col">Card03
+<div class="card">
+  <img src="img/flower3.jpg" class="card-img-top" alt="Foto de uma imagem">
+  <div class="card-body">
+    <h5 class="card-title text-success">Arranjo de flor</h5>
+    <p class="card-text">No nosso site voce encontra os melhores arranjo de flores</p>
+    <a href="#" class="btn btn-outline-Danger btn-lg">Compre já</a>
+  </div>
+</div>
+
+</div>
+<div calss="row">
+    <div class="col">
+        <h2 class="display-6 text-center">CONTATO</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d396076.14753025904!2d-47.64273230464862!3d-22.73504322471376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1677113438469!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div> 
+</div>
+
+</main>
+    <footer class="container bg-dark">
+        <div class="row">
+            <div class="col">
+                <h3 class="text-centr"></h3>
+            </div>
+            <div class="col text-end">
+                <img src="img/Icon instagram.png">
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
